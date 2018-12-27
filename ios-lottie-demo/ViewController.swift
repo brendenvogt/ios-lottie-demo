408473e5c0d730e6928e4333fabd1f885a5d9dd5
Brenden Vogt <bvogtdev@gmail.com>
2018-12-27 15:52:13 -0800

    Initial Commit

---
+++ ios-lottie-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-lottie-demo
+//
+//  Created by Brenden Vogt on 12/27/18.
+//  Copyright © 2018 BrendenVogt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
